e839acb90c07349bb7d782d977d18fde85b42cde
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-03-26 23:34:44 +0100

    clean up AppComponent

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ yarn create nx-workspace nx-angular-gitropolis --package-manager=yarn --preset=a
 yarn add @angular/material
 nx generate @angular/material:ng-add --theme=purple-green --typography --animations
 ```
+
+Clean up AppComponent:
+
+1. Remove NxWelcomeComponent.
+1. Delete app.component.spec.ts.
+1. Remove BrowserModule from AppComponent.imports.
--- apps/gitropolis-app/src/app/app.module.ts
+++ apps/gitropolis-app/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppComponent } from './app.component';
+
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
-  providers: [],
   bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [BrowserAnimationsModule],
 })
 export class AppModule {}
